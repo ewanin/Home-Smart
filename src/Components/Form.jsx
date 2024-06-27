df2b4e82e1f604e7392c397599736d5f07473a19
ewanin <70193546+ewanin@users.noreply.github.com>
2024-06-27 10:58:27 +0530

    Update Form.jsx

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -16,21 +16,21 @@ const Form = () => {
                 <img src={App} alt="App" width={240} className='hidden md:block drop-shadow-xl absolute left-1/2 -translate-x-1/2 max-w-16 xl:max-w-xs' />
                 <div className='absolute -bottom-18 left-1/2 -translate-x-1/2 -z-10 aspect-square md:border-8 border-amber-400 rounded-full md:w-72 lg:w-96'></div>
             </div>
-            <form action="" className="realtive border-8 border-neutral-900 p-6 rounded-lg grid gap-8 md:flex-1 md:max-w-lg my-4 md:my-12 lg:my-16 bg-white dark:bg-neutral-800 2-full">
+            <form action="" className="realtive border-2 border-neutral-900 p-6 rounded-lg grid gap-8 md:flex-1 md:max-w-lg my-4 md:my-12 lg:my-16 bg-white dark:bg-neutral-800 2-full">
                 <h2 id='contact' className='text-3xl font-bold'>Let's Connect</h2>
                 <div className='relative'>
-                    <input type="text" id='name' className='peer form-input w-full border-4 border-amber-400 rounded-md focus:ring-3 dark:ring-offset-2 focus:ring-amber-400 focus:border-amber-400 focus:outline-none dark:bg-amber-400 dark:text-neutral-900 placeholder-transparent' placeholder='Your Name' />
-                    <label htmlFor="name" className='text-neutral-500 text-sm font-bold uppercase absolute -top-4 left-2 -translate-y-1/2 peer-placeholder-shown:left-4 peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-neutral-900 peer-focus:-top-4 peer-focus:left-2 peer-focus:text-neutral-600 dark:peer-focus:text-neutral-300 transition-all '>Your Name</label>
+                    <input type="text" id='name' className='peer form-input w-full border-2 border-amber-400 rounded-md focus:ring-3 dark:ring-offset-2 focus:ring-amber-400 focus:border-amber-400 focus:outline-none dark:bg-amber-400 dark:text-neutral-900 placeholder-transparent' placeholder='Your Name' />
+                    <label htmlFor="name" className='text-neutral-500 text-[12px] py-1 uppercase absolute -top-4 left-2 -translate-y-1/2 peer-placeholder-shown:left-4 peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-neutral-900 peer-focus:-top-4 peer-focus:left-2 peer-focus:text-neutral-600 dark:peer-focus:text-neutral-300 transition-all '>Your Name</label>
                 </div>
 
                 <div className='relative'>
-                    <input type="email" id='email' className='peer form-input w-full border-4 border-amber-400 rounded-md focus:ring-3 dark:ring-offset-2 focus:ring-amber-400 focus:border-amber-400 focus:outline-none dark:bg-amber-400 dark:text-neutral-900 placeholder-transparent' placeholder='Your Email' />
-                    <label htmlFor="email" className='text-neutral-500 text-sm font-bold uppercase absolute -top-4 left-2 -translate-y-1/2 peer-placeholder-shown:left-4 peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-neutral-900 peer-focus:-top-4 peer-focus:left-2 peer-focus:text-neutral-600 dark:peer-focus:text-neutral-300 transition-all '>Your Email</label>
+                    <input type="email" id='email' className='peer form-input w-full border-2 border-amber-400 rounded-md focus:ring-3 dark:ring-offset-2 focus:ring-amber-400 focus:border-amber-400 focus:outline-none dark:bg-amber-400 dark:text-neutral-900 placeholder-transparent' placeholder='Your Email' />
+                    <label htmlFor="email" className='text-neutral-500 text-[12px] py-1 uppercase absolute -top-4 left-2 -translate-y-1/2 peer-placeholder-shown:left-4 peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-neutral-900 peer-focus:-top-4 peer-focus:left-2 peer-focus:text-neutral-600 dark:peer-focus:text-neutral-300 transition-all '>Your Email</label>
                 </div>
 
                 <div className='relative'>
-                    <textarea name="content" id="content" cols="20" rows="5" className='peer form-textarea resize-none w-full border-4 border-amber-400 rounded-md focus:ring-3 dark:ring-offset-2 focus:ring-amber-400 focus:border-amber-400 focus:outline-none dark:bg-amber-400 dark:text-neutral-900 placeholder-transparent' placeholder='How can we help?'></textarea>
-                    <label htmlFor="content" className='text-neutral-500 text-sm font-bold uppercase absolute -top-3 left-2 -translate-y-1/2 peer-placeholder-shown:left-4 peer-placeholder-shown:top-6 peer-placeholder-shown:text-neutral-900 peer-focus:-top-4 peer-focus:left-2 peer-focus:text-neutral-600 dark:peer-focus:text-neutral-300 transition-all '>How can we help?</label>
+                    <textarea name="content" id="content" cols="20" rows="5" className='peer form-textarea resize-none w-full border-2 border-amber-400 rounded-md focus:ring-3 dark:ring-offset-2 focus:ring-amber-400 focus:border-amber-400 focus:outline-none dark:bg-amber-400 dark:text-neutral-900 placeholder-transparent' placeholder='How can we help?'></textarea>
+                    <label htmlFor="content" className='text-neutral-500 text-[12px] py-1 uppercase absolute -top-3 left-2 -translate-y-1/2 peer-placeholder-shown:left-4 peer-placeholder-shown:top-6 peer-placeholder-shown:text-neutral-900 peer-focus:-top-4 peer-focus:left-2 peer-focus:text-neutral-600 dark:peer-focus:text-neutral-300 transition-all '>How can we help?</label>
                 </div>
 
                 <a href="#" className='p-2 px-6 bg-amber-400 w-max rounded-md shadow-xl hover:shadow-none dark:text-neutral-900 hover:text-neutral-600 flex gap-2'>
